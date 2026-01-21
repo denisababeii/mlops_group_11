60751fe8e26b0225284bdc425e3d4b7746de7972
MathildeElia <mathildeelia@yahoo.dk>
2026-01-21 16:05:18 +0100

    changed import order

--- src/mlops_group_11/data.py
+++ src/mlops_group_11/data.py
@@ -7,10 +7,9 @@ import zipfile
 from pathlib import Path
 from typing import Optional, Tuple
 
-import matplotlib
-import matplotlib.pyplot as plt
-
+import matplotlib 
 matplotlib.use("Agg")
+import matplotlib.pyplot as plt
 import pandas as pd
 import torch
 import typer
